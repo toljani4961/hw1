e59bea21c75e1390d04a8ea46496de77eb82944a
Anatolii lical <test@jnmail.com>
2021-08-10 14:15:13 +0300

    щоб не забути 10.08.21

--- js/learning.js
+++ js/learning.js
@@ -1,8 +1,30 @@
-let a = ['Киця', ',', 'я', 'Люблю', 'Тебе', ':)'];
-// console.log(a[0] + ' ' + a[1] + ' ' + a[2] + ' ' + a[3] + ' ' + a[4]);
+// // function first() {
+// //     setTimeout(function() {
+// //         console.log(1);
+// //     }, 1000);
+    
+// // }
+// // function second() {
+// //     console.log(2);
+// // }
+// // first();
+// // second();
 
-let s = '';
-for (let i = 0; i < a.length; i++) {
-s = s+a[i] + " ";
+// function learnJS(lang, callback) {
+//     console.log(`Я вчу: ${lang}`);
+//     callback();
+// }
+// learnJS(prompt("Що ти вчиш?", ""),function() {
+//     console.log('Я прошел урок!');
+// });
+
+// const str = prompt('', '');
+// const products = str.split(',');
+// console.log(products.join('; '));
+
+const arr = [1, 34, 22, 12, 9, 55, 153];
+arr.sort(compareNum);
+console.log(arr);
+function compareNum(a, b) {
+    return a - b;
 }
-console.log(s);
