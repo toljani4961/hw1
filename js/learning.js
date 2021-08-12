715d89556ab3c5e85511b91e9bd800f64da9b707
Anatolii lical <test@jnmail.com>
2021-08-12 10:14:32 +0300

    черговий комміт 11.08.21

--- js/learning.js
+++ js/learning.js
@@ -1,30 +1,94 @@
-// // function first() {
-// //     setTimeout(function() {
-// //         console.log(1);
-// //     }, 1000);
+// // // function first() {
+// // //     setTimeout(function() {
+// // //         console.log(1);
+// // //     }, 1000);
     
-// // }
-// // function second() {
-// //     console.log(2);
-// // }
-// // first();
-// // second();
+// // // }
+// // // function second() {
+// // //     console.log(2);
+// // // }
+// // // first();
+// // // second();
 
-// function learnJS(lang, callback) {
-//     console.log(`Я вчу: ${lang}`);
-//     callback();
+// // function learnJS(lang, callback) {
+// //     console.log(`Я вчу: ${lang}`);
+// //     callback();
+// // }
+// // learnJS(prompt("Що ти вчиш?", ""),function() {
+// //     console.log('Я прошел урок!');
+// // });
+
+// // const str = prompt('', '');
+// // const products = str.split(',');
+// // console.log(products.join('; '));
+
+// // const arr = [1, 34, 22, 12, 9, 55, 153];
+// // arr.sort(compareNum);
+// // console.log(arr);
+// // function compareNum(a, b) {
+// //     return a - b;
+// // }
+
+// // const obj = {
+// //     a: 5,
+// //     b: 3
+// // };
+// // const copy = obj; // link of obj 
+// // copy.a = 999;
+// // console.log(copy);
+// // console.log(obj);
+
+// function doingCopy(mainObj) {
+//     let objCopy = {};
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
 // }
-// learnJS(prompt("Що ти вчиш?", ""),function() {
-//     console.log('Я прошел урок!');
-// });
+// const numbers = {
+//     a: 1,
+//     b: 54,
+//     c: 8,
+//     d: {
+//         e: 84,
+//         f: 4
+//     }
+// };
+// const newNumbers = doingCopy(numbers);
+// newNumbers.a = 13333;
+// // console.log(newNumbers);
+// // console.log(numbers);
 
-// const str = prompt('', '');
-// const products = str.split(',');
-// console.log(products.join('; '));
+// let add = {
+//     v: 3323,
+//     n: 3222
+// };
+// // console.log(Object.assign(numbers, add));
 
-const arr = [1, 34, 22, 12, 9, 55, 153];
-arr.sort(compareNum);
-console.log(arr);
-function compareNum(a, b) {
-    return a - b;
-}
+// const clone = Object.assign({}, add);
+// clone.v = 0.222;
+// console.log(add);
+// console.log(clone);
+
+
+//  const oldArray = ['a', 'b', 'c'];
+//  const newArray = oldArray.slice();
+//  oldArray[4] = 'asdfasdfasdf';
+//  console.log(oldArray);
+//  console.log(newArray);
+ 
+const soldier = {
+    armor: 311,
+    helth: 100,
+    sayHello: function() {
+        console.log("hello");
+    }
+};
+const John = Object.create(soldier);
+// const John = {
+//     helth: 100
+// };
+// John.__proto__ = soldier;
+// console.log(John.helth);
+John.sayHello();
--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
-let numberOfFilms;
-function start() {
-    numberOfFilms = +prompt("Скільки фільмів ви дивились?", "");
-    while (numberOfFilms == ' ' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt("Скільки фільмів ви дивились?", '2');
-    }
-}
-start();
+// let numberOfFilms;
+// function start() {
+//     numberOfFilms = +prompt("Скільки фільмів ви дивились?", "");
+//     while (numberOfFilms == ' ' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = prompt("Скільки фільмів ви дивились?", '2');
+//     }
+// }
+// start();
 
 const personalMovieD8 = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-function questionsSycle() {
+    privat: false,
+    start: function() {
+        personalMovieD8.count = +prompt("Скільки фільмів ви дивились?", "");
+        while (personalMovieD8.count == ' ' || personalMovieD8.count == null || isNaN(personalMovieD8.count)) {
+            personalMovieD8.count = prompt("Скільки фільмів ви дивились?", '2');
+        }
+    },
+    questionsSycle: function() {
     for (let i = 0; i < 1;  i++) {
         const a = prompt("один из последних просмотренніх фильмов", ''),
               b = +prompt("оцінка", '');
@@ -29,50 +33,91 @@ function questionsSycle() {
                   b = +prompt("оцінка", '');
                   personalMovieD8.genres[a] = b;
             console.log("Repeat of a and b was initialized by Error");
-        }
-    }
-}
-questionsSycle();
-
-function questionsSycleCheck() {
-    if (personalMovieD8.count < 10) {
-            console.log('просмотренно довольно мало фильмов');
-        } else if (personalMovieD8.count > 10 && personalMovieD8.count < 30) {
-            console.log('ви класичний користувач');
-        } else if (personalMovieD8.count >= 30) {
-            console.log('ви кіноман');
-        } else {
-            console.log('error2222');
-        }
-        
-        while (true) {
-            let repQw = confirm("Ви дивились ще якісь фільми?", "");
-            if(!repQw) {
-                break;
             }
-            const c = prompt("один из последних просмотренніх фильмов", []),
-                  d = +prompt("оцінка", ''); 
-                  personalMovieD8.genres[c] = d;
         }
- }
- questionsSycleCheck();
+    },
+    questionsSycleCheck: function() {
+            if (personalMovieD8.count < 10) {
+                    console.log('просмотренно довольно мало фильмов');
+                } else if (personalMovieD8.count > 10 && personalMovieD8.count < 30) {
+                    console.log('ви класичний користувач');
+                } else if (personalMovieD8.count >= 30) {
+                    console.log('ви кіноман');
+                } else {
+                    console.log('error2222');
+                }
+                
+                while (true) {
+                    let repQw = confirm("Ви дивились ще якісь фільми?", "");
+                    if(!repQw) {
+                        break;
+                    }
+                    const c = prompt("один из последних просмотренніх фильмов", []),
+                          d = +prompt("оцінка", ''); 
+                          personalMovieD8.genres[c] = d;
+                }
+         }
+};
+personalMovieD8.start();
 
- function showMyBD(hidden) {
-     if(!hidden) {
-        console.log("not hidden");
-     } else {
-         console.log("rivar явно true and hidden");
-     }
- }
-showMyBD(personalMovieD8.privat);
+// function questionsSycle() {
+//     for (let i = 0; i < 1;  i++) {
+//         const a = prompt("один из последних просмотренніх фильмов", ''),
+//               b = +prompt("оцінка", '');
+//                 // personalMovieD8.genres[a] = b;
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieD8.genres[a] = b;
+//         console.log("done");
+//         // i--; 
+//         } else {
+//             const a = prompt("один из последних просмотренніх фильмов", ''),
+//                   b = +prompt("оцінка", '');
+//                   personalMovieD8.genres[a] = b;
+//             console.log("Repeat of a and b was initialized by Error");
+//         }
+//     }
+// }
+// questionsSycle();
 
-function writeYorGenres() {
-    for( i = 1; i <= 3; i++) {
-        // let qwGener = prompt(`Ваш улюблений жанр під номером ${i}`);
-        personalMovieD8.genres[i - 1] = prompt(`Ваш улюблений жанр під номером ${i}`);
-    }
-}
-writeYorGenres();
+// function questionsSycleCheck() {
+//     if (personalMovieD8.count < 10) {
+//             console.log('просмотренно довольно мало фильмов');
+//         } else if (personalMovieD8.count > 10 && personalMovieD8.count < 30) {
+//             console.log('ви класичний користувач');
+//         } else if (personalMovieD8.count >= 30) {
+//             console.log('ви кіноман');
+//         } else {
+//             console.log('error2222');
+//         }
+        
+//         while (true) {
+//             let repQw = confirm("Ви дивились ще якісь фільми?", "");
+//             if(!repQw) {
+//                 break;
+//             }
+//             const c = prompt("один из последних просмотренніх фильмов", []),
+//                   d = +prompt("оцінка", ''); 
+//                   personalMovieD8.genres[c] = d;
+//         }
+//  }
+//  questionsSycleCheck();
+
+//  function showMyBD(hidden) {
+//      if(!hidden) {
+//         console.log("not hidden");
+//      } else {
+//          console.log("rivar явно true and hidden");
+//      }
+//  }
+// showMyBD(personalMovieD8.privat);
+
+// function writeYorGenres() {
+//     for( i = 1; i <= 3; i++) {
+//         // let qwGener = prompt(`Ваш улюблений жанр під номером ${i}`);
+//         personalMovieD8.genres[i - 1] = prompt(`Ваш улюблений жанр під номером ${i}`);
+//     }
+// }
+// writeYorGenres();
 
 //     // if (a != null && b != null && a != '' && b != '' && a.length < 50) {
 //     //     personalMovieD8.genres[a] = b;
